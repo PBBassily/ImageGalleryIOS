47cdbd7f5b50d84e009d12350068d2e258f4bfa9
PBBassily <paulaboules@Pharos-Mac-Sever.local>
2018-09-26 12:36:13 +0200

    drop permission

---
+++ ImageGallery/GalleryImage.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryImage.swift
+//  ImageGallery
+//
+//  Created by Paula Boules on 9/26/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import Foundation
+
+struct GalleryImage {
+    
+    var url : URL
+    
+    init(url : URL){
+        self.url = url
+    }
+}
