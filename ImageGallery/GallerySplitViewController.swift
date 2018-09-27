c834eaf5b78f5bdd05a0ce630102d64c763b9fb0
PBBassily <paulaboules@pharos-mac-sever.local>
2018-09-27 10:16:48 +0200

    overlay

---
+++ ImageGallery/GallerySplitViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GallerySplitViewController.swift
+//  ImageGallery
+//
+//  Created by Paula Boules on 9/27/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+class GallerySplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewWillLayoutSubviews() {
+        if  self.preferredDisplayMode != .primaryOverlay {
+            self.preferredDisplayMode = .primaryOverlay
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
